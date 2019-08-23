e8ce54c4c5badbc66313f08e7ef9073b510ee77d
Miguel Motta <miguelmotta@MacBook-Pro-de-Miguel.local>
2019-08-23 10:30:31 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+void mayor(void){
+
+    int a , b , c ;
+
+    std::cout<<"Digite un número: " ; std::cin >> a ;
+    std::cout<<"Digite un número: " ; std::cin >> b ;
+    std::cout<<"Digite un número: " ; std::cin >> c ;
+
+    if (a > b and a > c ){
+        std::cout << a << " es el número mayor" << std::endl;
+    }
+    else if (b > a and b > c ){
+        std::cout << b << " es el número mayor" << std::endl;
+    }
+    else {
+        std::cout << c << " es el número mayor" << std::endl;
+    }
+}
+
 int main(){
 
-    std::cout<<"prueba"<<std::endl;
+    mayor();
+
     return 0;
 }
